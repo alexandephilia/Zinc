2847d9538973dd075a40c2d99594bb71d98b56ab
alexandephilia <freemasonga1@gmail.com>
2024-12-31 12:15:03 +0700

    Update styles2.css with latest changes

--- jupiter-swap.js
+++ jupiter-swap.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
             defaultExplorer: "Solscan",
             containerStyles: {
                 width: '100%',
-                minHeight: window.innerWidth <= 768 ? '550px' : '600px',
+                minHeight: '100%',
                 border: 'none', 
                 borderRadius: '0',
                 background: 'transparent',
